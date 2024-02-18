addeec0fde08dab7e705f1b408d6a59dd822e5c5
erp14 <erp14@erp14-VirtualBox>
2024-02-18 19:34:31 +0300

    feat: Initialize App

---
+++ README.md
@@ -0,0 +1,7 @@
+## my-app
+
+for mangingin hospital manger
+
+#### License
+
+MIT
---
+++ my_app/config/desktop.py
@@ -0,0 +1,10 @@
+from frappe import _
+
+def get_data():
+	return [
+		{
+			"module_name": "my-app",
+			"type": "module",
+			"label": _("my-app")
+		}
+	]
